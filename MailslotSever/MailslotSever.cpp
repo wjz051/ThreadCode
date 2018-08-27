a5a2d56162bfe6a86d430422cef2af6debc7ae02
wjz051 <422358883@qq.com>
2018-08-27 17:43:59 +0800

    C++线程代码

---
+++ MailslotSever/MailslotSever.cpp
@@ -0,0 +1,35 @@
+#include<windows.h>											//包含头文件
+#include<stdio.h>
+
+int main()														//主函数
+{
+	/*1.创建油槽*/
+	HANDLE mail;												//定义邮槽句柄
+																//创建邮槽
+	mail = CreateMailslot("\\\\.\\mailslot\\my", 0, MAILSLOT_WAIT_FOREVER, NULL);
+
+	if (mail == INVALID_HANDLE_VALUE)								//判断邮槽句柄
+	{
+		printf("创建邮槽失败！\r\n");								//提示信息
+		return 0;
+	}
+	else
+	{
+		printf("创建邮槽成功，正在读取数据……！\r\n");
+
+		/*2.读取数据，循环等待数据*/
+		char text[200];												//定义字符数组
+		DWORD readtext;												//获取实际读取值
+		while (1)
+		{
+
+			if (ReadFile(mail, text, 200, &readtext, NULL))					//读取数据
+			{
+				printf(text);												//显示数据
+			}
+		}
+	}
+	Sleep(100000);
+	CloseHandle(mail);
+	return 0;
+}
---
+++ NamedPipeSever/NamedPipeSever.cpp
@@ -0,0 +1,34 @@
+#include<windows.h>								//包含头文件
+#include<stdio.h>
+
+int main()
+{
+	/*1.创建管道*/
+	HANDLE hpip;								//定义命名管道句柄
+	hpip = CreateNamedPipe("\\\\.\\pipe\\pipename", PIPE_ACCESS_DUPLEX,
+		PIPE_TYPE_BYTE, PIPE_UNLIMITED_INSTANCES, 1024, 1024, 0,
+		NULL);					 	//创建命名管道 
+	printf("创建管道成功，正在等待客户端连接！\r\n");
+
+	/*2.等待连接*/
+	OVERLAPPED ovi = { 0 };							//定义结构体变量
+	if (::ConnectNamedPipe(hpip, &ovi))				//等待客户端的连接请求
+	{
+		printf("客户端连接成功！\r\n");
+		printf("正在读取数据！\r\n");					//提示信息
+
+		/*3.读取数据*/
+		char buf[200];								//定义数据缓冲区
+		DWORD readbuf;									//获取实际读取字节数
+		if (ReadFile(hpip, buf, 200, &readbuf, NULL))		//读取管道数据
+		{
+			printf("数据读取成功\r\n");						//提示信息
+			printf("读取的数据是：%s\r\n", buf);
+		}
+		else
+		{
+			printf("数据读取失败\r\n");
+		}
+	}
+	return 0;
+}
